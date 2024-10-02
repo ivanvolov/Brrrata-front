41d65559070087a4974c743894b01436a85e2fdf
IVa <vivan.volovik@gmail.com>
2024-10-02 12:04:15 +0200

    added cheese testnet

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,28 @@
 // Import necessary modules
 import '@rainbow-me/rainbowkit/styles.css';
-import { getDefaultConfig, RainbowKitProvider } from '@rainbow-me/rainbowkit';
+import { getDefaultConfig, RainbowKitProvider, Chain } from '@rainbow-me/rainbowkit';
 import { WagmiProvider } from 'wagmi';
 import { mainnet, polygon, optimism, arbitrum, base } from 'wagmi/chains';
 import { QueryClientProvider, QueryClient } from "@tanstack/react-query";
 import { ConnectButton } from '@rainbow-me/rainbowkit';
 
+const cheeseChainTestnet = {
+   id: 9209108,
+   name: 'CheeseChain Testnet',
+   iconUrl: 'https://misc-bucket.caldera.xyz/photo_5030561642252971496_y.jpg',
+   iconBackground: '#fff',
+   nativeCurrency: { name: 'TestCheese', symbol: 'CHEESE', decimals: 18 },
+   rpcUrls: {
+     default: { http: ['https://cheesechain-testnet.rpc.caldera.xyz/http'] },
+   }
+ } as const satisfies Chain;
+ 
+
 // Set Config
 const config = getDefaultConfig({
    appName: 'Brrrata Web App',
    projectId: '1590ddedb144ba56c0842d3b13c4297b',
-   chains: [mainnet, polygon, optimism, arbitrum, base],
+   chains: [mainnet, cheeseChainTestnet],
    ssr: false,
 });
 
