1dc488599d2abb7151e28794118e401d8284d466
IVa <vivan.volovik@gmail.com>
2024-10-21 15:21:00 +0200

    mainnet

--- src/web3/wagmiConfig.ts
+++ src/web3/wagmiConfig.ts
@@ -1,7 +1,6 @@
 // Import necessary modules
 import '@rainbow-me/rainbowkit/styles.css';
 import { getDefaultConfig, Chain } from '@rainbow-me/rainbowkit';
-import { mainnet } from 'wagmi/chains';
 import { createStorage, deserialize } from 'wagmi';
 
 const cheeseChainTestnet = {
@@ -15,6 +14,18 @@ const cheeseChainTestnet = {
   },
 } as const satisfies Chain;
 
+// https://cheesechain.hub.caldera.xyz/
+const cheeseChain = {
+  id: 383353,
+  name: 'CheeseChain',
+  iconUrl: 'https://misc-bucket.caldera.xyz/photo_5030561642252971496_y.jpg',
+  iconBackground: '#fff',
+  nativeCurrency: { name: 'CHEESE', symbol: 'CHEESE', decimals: 18 },
+  rpcUrls: {
+    default: { http: ['https://cheesechain.calderachain.xyz/http'] },
+  },
+} as const satisfies Chain;
+
 export const storage = createStorage({
   deserialize,
   storage: localStorage,
@@ -23,7 +34,7 @@ export const storage = createStorage({
 export const config = getDefaultConfig({
   appName: 'Brrrata Web App',
   projectId: '1590ddedb144ba56c0842d3b13c4297b',
-  chains: [cheeseChainTestnet],
+  chains: [cheeseChain],
   ssr: false,
   storage: storage,
 });
